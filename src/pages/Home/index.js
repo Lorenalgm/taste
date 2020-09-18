ac77d0ceb73352b8528daa2001599a291a978f1a
Lorenalgm <lorena@proesc.com>
2020-09-18 13:02:28 -0300

    Criado html da seção hero

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,28 @@ import React from 'react'
 
 function Home(){
     return(
-        <p>Oiiiiii</p>
+        <div className="container">
+            <header>
+                <h1>Taste</h1>
+                <div className="car">
+                </div>
+            </header>
+            <main>
+                <div className="section-hero">
+                    <div className="hero-content">
+                        <h1>Comidas deliciosas estão esperando por você</h1>
+                        <a href="#">Ver menu</a>
+
+                        <div className="food-options">
+                            <div className="option">x</div>
+                            <div className="option">y</div>
+                            <div className="option">z</div>
+                        </div>
+                    </div>
+                    <img src="" alt="Food" />
+                </div>
+            </main>
+        </div>
     )
 }
 
