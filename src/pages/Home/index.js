f011be53aac23e1e15aa12a65740fad18bbfbfe7
Lorenalgm <lorena@proesc.com>
2020-09-19 23:08:20 -0300

    Started styles from home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import image1 from '../../assets/image1.png';
 import image2 from '../../assets/image2.png';
 import image4 from '../../assets/image4.png';
+import './style.css';
 
 function Home(){
     return(
